30555ca1b3b81a85127b964b3c1def14899680dd
detalhe <updudu@hotmail.com>
2024-06-08 09:49:55 -0300

    New fixes and enhancements

--- index.js
+++ index.js
@@ -1,47 +1,53 @@
-require('dotenv').config();
-const express = require('express');
-const multer = require('multer');
-const path = require('path');
-const fs = require('fs');
-const { GoogleGenerativeAI, HarmCategory, HarmBlockThreshold } = require("@google/generative-ai");
+require("dotenv").config();
+const express = require("express");
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
+const {
+    GoogleGenerativeAI,
+    HarmCategory,
+    HarmBlockThreshold,
+} = require("@google/generative-ai");
 
 const app = express();
 const upload = multer({ storage: multer.memoryStorage() });
 const MODEL_NAME = "gemini-1.5-pro";
 const API_KEY = process.env.GOOGLE_API_KEY;
 
-app.set('view engine', 'ejs');
-app.set('views', `${__dirname}/views`);
+app.set("view engine", "ejs");
+app.set("views", `${__dirname}/views`);
 app.use(express.static(`${__dirname}/public`));
 
-
-app.get('/', (req, res) => {
-    res.render('index', { carInfo: null });
+app.get("/", (req, res) => {
+    res.render("index", { carInfo: null });
 });
 
-app.post('/upload', upload.single('image'), async (req, res) => {
+app.post("/upload", upload.single("image"), async (req, res) => {
     const imageBuffer = req.file ? req.file.buffer : null;
     if (!imageBuffer) {
-        return res.status(400).json({ error: 'No image file provided' });
+        return res.status(400).json({ error: "No image file provided" });
     }
 
     try {
         const carInfoJson = await getCarInfo(imageBuffer);
-        console.log('Server response:', carInfoJson);
+        console.log("Server response:", carInfoJson);
         const carInfo = JSON.parse(carInfoJson);
         if (carInfo.error) {
             res.status(400).json({ error: carInfo.error });
         } else {
-            carInfo.imageBase64 = imageBuffer.toString('base64');
             res.json({ carInfo });
         }
     } catch (error) {
-        console.error('Error:', error);
-        if (error.name === 'GoogleGenerativeAIFetchError') {
-            console.error('Google API Error:', error);
-            res.status(500).json({ error: 'A server error occurred at Google\'s API. Please retry later.' });
+        console.error("Error:", error);
+        if (error.name === "GoogleGenerativeAIFetchError") {
+            console.error("Google API Error:", error);
+            res.status(500).json({
+                error: "A server error occurred at Google's API. Please retry later.",
+            });
         } else {
-            res.status(500).json({ error: 'An unexpected error occurred on the server.' });
+            res.status(500).json({
+                error: "An unexpected error occurred on the server.",
+            });
         }
     }
 });
@@ -79,24 +85,48 @@ const getCarInfo = async (imageBuffer) => {
     const parts = [
         {
             text: `Accurately identify the vehicle model, manufacturer, color, and possible year with your analysis. Please respond in the following JSON format:
+
       {
         "vehicle": {
           "manufacturer": "string",
           "model": "string",
           "color": "string",
-          "possible-year": "number"
+          "year": "string"
         }
       }
-      If the image does not contain a car, respond in this format:
+
+      If the image does not contain a vehicle, respond in this format:
       {
         "error": "The image does not contain a vehicle."
-      }`
+      }
+
+      Example responses:
+
+      For a successful identification:
+      {
+        "vehicle": {
+          "manufacturer": "Toyota",
+          "model": "Camry",
+          "color": "White",
+          "year": "2020"
+        }
+      }
+
+      If the vehicle's year cannot be exactly determined, provide the range in the format "YYYY-YYYY" (e.g., "2002-2006"). Example:
+      {
+        "vehicle": {
+          "manufacturer": "Honda",
+          "model": "Civic",
+          "color": "Black",
+          "year": "2002-2006"
+        }
+      }`,
         },
         {
             inlineData: {
                 mimeType: "image/jpeg",
-                data: imageBuffer.toString("base64")
-            }
+                data: imageBuffer.toString("base64"),
+            },
         },
     ];
 
@@ -107,10 +137,13 @@ const getCarInfo = async (imageBuffer) => {
     });
 
     const response = result.response;
-    return response.text().replace(/```json/g, '').replace(/```/g, '');
+    return response
+        .text()
+        .replace(/```json/g, "")
+        .replace(/```/g, "");
 };
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`App is running on port ${port}`);
+    console.log(`App is running on port ${port}`);
 });
--- public/js/script.js
+++ public/js/script.js
@@ -1,107 +1,135 @@
-const fileInput = document.getElementById('fileInput');
-const uploadButton = document.getElementById('uploadButton');
-const runButton = document.getElementById('runButton');
-const fileInfo = document.getElementById('fileInfo');
-const fileName = document.getElementById('fileName');
-const removeButton = document.getElementById('removeButton');
+const fileInput = document.getElementById("fileInput");
+const uploadButton = document.getElementById("uploadButton");
+const runButton = document.getElementById("runButton");
+const fileInfo = document.getElementById("fileInfo");
+const fileName = document.getElementById("fileName");
+const removeButton = document.getElementById("removeButton");
 
-const carImage = document.getElementById('carImage');
-const carMake = document.getElementById('carMake');
-const carModel = document.getElementById('carModel');
-const carColor = document.getElementById('carColor');
-const carYear = document.getElementById('carYear');
+const carImage = document.getElementById("carImage");
+const carMake = document.getElementById("carMake");
+const carModel = document.getElementById("carModel");
+const carColor = document.getElementById("carColor");
+const carYear = document.getElementById("carYear");
+const carMakeLogo = document.getElementById("carMakeLogo");
 
-const errorMessage = document.getElementById('errorMessage');
+const errorMessage = document.getElementById("errorMessage");
 
 let isRunning = false; // Flag variable
 
-fileInput.addEventListener('change', function () {
+fileInput.addEventListener("change", function () {
     if (fileInput.files.length > 0) {
-        uploadButton.style.display = 'none';
-        runButton.style.display = 'inline-block';
-        fileInfo.style.display = 'flex';
-        fileName.textContent = fileInput.files[0].name.slice(0, 20) + (fileInput.files[0].name.length > 20 ? '...' : '');
-        errorMessage.style.display = 'none'; // Clear error message
+        uploadButton.style.display = "none";
+        runButton.style.display = "inline-block";
+        fileInfo.style.display = "flex";
+        fileName.textContent =
+            fileInput.files[0].name.slice(0, 20) +
+            (fileInput.files[0].name.length > 20 ? "..." : "");
+        errorMessage.style.display = "none"; // Clear error message
     }
 });
 
-removeButton.addEventListener('click', function () {
-    fileInput.value = '';
-    uploadButton.style.display = 'inline-block';
-    runButton.style.display = 'none';
-    fileInfo.style.display = 'none';
+removeButton.addEventListener("click", function () {
+    fileInput.value = "";
+    uploadButton.style.display = "inline-block";
+    runButton.style.display = "none";
+    fileInfo.style.display = "none";
     isRunning = false; // Reset the flag when removing the file
-    errorMessage.style.display = 'none'; // Clear error message
-    resetCarInfo(); // Reset car info fields
+    errorMessage.style.display = "none"; // Clear error message
 });
 
-runButton.addEventListener('click', function (event) {
+function formatCarMakeForLogo(make) {
+    return make.toLowerCase().replace(/ /g, "%20");
+}
+
+async function checkLogoExists(url) {
+    try {
+        const response = await fetch(url, { method: "HEAD" });
+        return response.ok;
+    } catch (error) {
+        console.error("Error checking logo existence:", error);
+        return false;
+    }
+}
+
+runButton.addEventListener("click", function (event) {
     event.preventDefault();
     if (!isRunning) {
         isRunning = true;
-        runButton.textContent = 'Running...';
-        runButton.classList.add('generating');
+        runButton.textContent = "Running...";
+        runButton.classList.add("generating");
 
         const formData = new FormData();
-        formData.append('image', fileInput.files[0]);
+        formData.append("image", fileInput.files[0]);
 
-        fetch('/upload', {
-            method: 'POST',
+        fetch("/upload", {
+            method: "POST",
             body: formData,
         })
-        .then(response => {
-            if (!response.ok) {
-                return response.json().then(data => {
-                    throw new Error(data.error);
-                });
-            }
-            return response.json();
-        })
-        .then(data => {
-            if (data.carInfo) {
-                // Update car card with the new data
-                carImage.src = 'data:image/jpeg;base64,' + data.carInfo.imageBase64;
-                carMake.textContent = data.carInfo.vehicle.manufacturer;
-                carModel.textContent = data.carInfo.vehicle.model;
-                carColor.textContent = data.carInfo.vehicle.color;
-                carYear.textContent = data.carInfo.vehicle['possible-year'];
-                errorMessage.style.display = 'none'; // Clear error message
-            } else {
-                // Handle unexpected response format
-                throw new Error('Unexpected response from the server.');
-            }
-            resetUploadForm();
-            scrollToDetails();
-        })
-        .catch(error => {
-            console.error('Error:', error);
-            errorMessage.textContent = error.message; // Display error message
-            errorMessage.style.display = 'block'; // Show error message
-            resetUploadForm();
-            resetCarInfo(); // Reset car info fields
-            scrollToDetails();
-        });
+            .then((response) => {
+                if (!response.ok) {
+                    return response.json().then((data) => {
+                        throw new Error(data.error);
+                    });
+                }
+                return response.json();
+            })
+            .then(async (data) => {
+                if (data.carInfo) {
+                    // Update car card with the new data
+                    const make = data.carInfo.vehicle.manufacturer;
+                    carMake.textContent = make;
+                    carModel.textContent = data.carInfo.vehicle.model;
+                    carColor.textContent = data.carInfo.vehicle.color;
+                    carYear.textContent = data.carInfo.vehicle.year;
+
+                    // Update car make logo
+                    const logoUrl = `https://raw.githubusercontent.com/dangnelson/car-makes-icons/2a7f574ce813e1eeddcca955c87847bc5baa28b6/svgs/${formatCarMakeForLogo(
+                        make
+                    )}.svg`;
+                    const logoExists = await checkLogoExists(logoUrl);
+                    if (logoExists) {
+                        carMakeLogo.src = logoUrl;
+                        carMakeLogo.style.display = "block";
+                    } else {
+                        carMakeLogo.style.display = "none";
+                    }
+
+                    // Display the user-uploaded image after processing
+                    const reader = new FileReader();
+                    reader.onload = function (e) {
+                        carImage.src = e.target.result;
+                    };
+                    reader.readAsDataURL(fileInput.files[0]);
+
+                    errorMessage.style.display = "none"; // Clear error message
+                } else {
+                    // Handle unexpected response format
+                    throw new Error("Unexpected response from the server.");
+                }
+                resetUploadForm();
+                scrollToDetails();
+            })
+            .catch((error) => {
+                console.error("Error:", error);
+                errorMessage.textContent = error.message; // Display error message
+                errorMessage.style.display = "block"; // Show error message
+                resetUploadForm();
+                resetCarInfo(); // Reset car info fields
+                scrollToDetails();
+            });
     }
 });
 
 function resetUploadForm() {
-    fileInput.value = ''; 
-    uploadButton.style.display = 'inline-block'; 
-    runButton.style.display = 'none'; 
-    fileInfo.style.display = 'none'; 
-    runButton.textContent = 'Run'; 
-    runButton.classList.remove('generating'); 
+    fileInput.value = "";
+    uploadButton.style.display = "inline-block";
+    runButton.style.display = "none";
+    fileInfo.style.display = "none";
+    runButton.textContent = "Run";
+    runButton.classList.remove("generating");
     isRunning = false;
 }
 
-function resetCarInfo() {
-    carImage.src = '/img/default.jpg'; // Reset to default image
-    carMake.textContent = 'N/A';
-    carModel.textContent = 'N/A';
-    carColor.textContent = 'N/A';
-    carYear.textContent = 'N/A';
-}
-
 function scrollToDetails() {
-    document.querySelector('.car-card-section').scrollIntoView({ behavior: 'smooth' });
+    document.querySelector(".car-color").scrollIntoView({ behavior: "smooth" });
 }
